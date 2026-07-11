1269648293ec60fe49a5f28d0b22a4fe047a2bc0
Saltiefr <sslexic@gmail.com>
2026-07-11 22:42:23 +0530

    Fix import case for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
 import Marquee from "./components/Marquee";
 import Stats from "./components/Stats";
